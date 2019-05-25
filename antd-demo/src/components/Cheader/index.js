2a64c56b8d3820a822d560f9777d21dfafcee7cc
taishuang4687 <18845772690@163.com>
2019-05-25 22:09:26 +0800

    antd-demo

--- antd-demo/src/components/Cheader/index.js
+++ antd-demo/src/components/Cheader/index.js
@@ -4,18 +4,33 @@ import {
     Col
 } from 'antd'
 import './index.css'
+import formatDate from './../../utils/formatDate'
 export default class Cheader extends Component {
+    constructor(params) {
+        super(params);
+        this.state = {
+            timer:''
+        }
+    }
+    componentWillMount(){
+        setInterval(() => {
+            let now = new Date();
+            let timer = formatDate(now);
+            console.log(timer)
+            this.setState({
+                timer
+            })
+        }, 1000);
+    }
     render() {
         return (
             <div>
-                <Row>
-                    <Col className='header-top'>
-                        <span className='header-text'>欢迎，xxx</span>
-                        <a href='#'>退出</a>
-                    </Col>
-                    <Col>
-                        
-                    </Col>
+                <Row className='header-top'>
+                    <span className='header-text'>欢迎，xxx</span>
+                    <a href='#'>退出</a>
+                </Row>
+                <Row className="header-date">
+                    {this.state.timer}
                 </Row>
             </div>
         )
