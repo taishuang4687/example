f5ec54f616f8624e09dd0eacad8d1dd161023a61
taishuang4687 <18845772690@163.com>
2019-06-26 22:31:21 +0800

    myblog

--- myblog/app.js
+++ myblog/app.js
@@ -3,7 +3,9 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var cookieSession = require('cookie-session');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -18,6 +20,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cookieSession({
+  name:'session',
+  keys:['1234567890'],
+  maxAge:24 * 60 * 60 * 1000
+}))
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- myblog/controllers/blog.js
+++ myblog/controllers/blog.js
@@ -1,12 +1,12 @@
 var Blog_model=require('../models/blog_model.js'); 
 exports.index=function(req,res,next){
-    Blog_model.sel_all(function(err,data){
-		//console.log(data);
-		res.render("index_logined",{
-			'blogs':data,
-			'sess':req.session,
-		})
-    });
-    
+    //console.log(req.session);
+    var uid = req.session.USER_ID;
+    Blog_model.sel_id_by_data(uid,function(err,data){
+        res.render('index_logined',{
+            'blogs':data,
+            'sess':req.session
+        });
+    })
     //res.render('index.ejs');
 }
